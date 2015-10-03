37191c60c8e2d19ae896cce3b8c978f92d2367d4
Nikita Mashukov <mashukovnikita@gmail.com>
2015-10-03 22:41:37 +0300

    name change for the session feature

--- src/main/java/ru/ferdibiflator/javaeeTraining/controller/HelloController.java
+++ src/main/java/ru/ferdibiflator/javaeeTraining/controller/HelloController.java
@@ -4,25 +4,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class HelloController extends HttpServlet{
     public static final String PAGE_OK = "/hello.jsp";
     public static final String PAGE_ERROR = "/error.jsp";
     public static final String PARAM_NAME = "name";
-    public static final String ATTR_NAME_TO_VIEW = "name";
+    public static final String ATTR_NAME = "name";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         System.out.println(">> HelloController - doGet");
 
         String name = req.getParameter(PARAM_NAME);
+        HttpSession session = req.getSession();
         if(name != null) {
-            req.setAttribute(ATTR_NAME_TO_VIEW, name);
-            getServletContext().getRequestDispatcher(PAGE_OK).forward(req, res);
+            session.setAttribute(ATTR_NAME, name);
         }
         else {
-            res.sendRedirect(req.getContextPath() + PAGE_ERROR);
+            if(session.getAttribute(ATTR_NAME) == null) {
+                session.setAttribute(ATTR_NAME, "stranger");
+            }
         }
+        getServletContext().getRequestDispatcher(PAGE_OK).forward(req, res);
     }
 }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -1,10 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Hello page</title>
 </head>
 <body>
-    <br/>Hello, <%=request.getParameter("name")%>!
     <br/>Hello, ${name}!
+    
+    <c:if test="${name == 'stranger'}">
+    <form method="GET" action="hello.do">
+        <input type="text" name="name" size="20" value="I'm not stranger">
+        <input type="submit">
+    </form>
+    </c:if>
 </body>
 </html>
